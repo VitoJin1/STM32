73ca5ed99a99c7a879a7a9cf90c92804b1ba1cd3
vito.jin <icejintao@gmail.com>
2021-05-11 15:11:50 +0800

    add first order low pass filter sturcture in c

--- filter.c
+++ filter.c
@@ -18,3 +18,42 @@ float ButterWorthFilter( float x )
     yBuf[0]=input_acc-outout_acc;
     return yBuf[0];
 }
+
+first order low pass filter
+void LPF_1orderRC_F_init(LPF_1orderRC_F *v)
+{
+    v->Vi=0.0;
+    v->Vo_last=0.0;
+    v->Vo=0.0;
+    v->Fcutoff=30;
+    v->Fs=1000;
+}
+float LPF_1orderRC_F_FUNC(LPF_1orderRC_F *v)
+{
+    float RC, Cof1,Cof2;
+    RC=(float)1.0/2.0/PI/v->Fcutoff;
+    Cof1=1/(1+RC*v->Fs);
+    Cof2=RC*v->Fs/(1+RC*v->Fs);
+    v->Vo=Cof1*v->Vi+Cof2*v->Vo_last;
+    v->Vo_last=v->Vo;
+    return v->Vo;
+}
+typedef struct{
+    float Vi;
+    float Vo_last;
+    float Vo;
+    float Fcutoff;
+    float Fs;
+}LPF_1orderRC_F;
+
+大小端模式测试
+int checkSystem( )
+{
+union check
+{
+int i;
+char ch;
+} c;
+c.i = 1;
+return (c.ch ==1);
+}
